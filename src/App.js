170e1c357dd194e45922b134d44758a9dbc1bda2
LeuthsoulineChanthathirath <r.leuthsouline@gmail.com>
2018-10-03 16:51:01 +0200

    header slogan

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from "./Header";
+import "./Header.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <a>-</a>
+        <Header />
       </div>
     );
   }
