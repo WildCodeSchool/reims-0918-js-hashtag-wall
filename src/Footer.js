53579689a9f79515135feb3422d260df2ea7ce1b
Benoit Niveau <be.niveau@hotmail.fr>
2018-10-03 15:41:14 +0200

    modif PR

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,11 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => (
   <footer>
     <div class="container">
       <ul class="row">
         <li class="col-6">
-          {" "}
           <img
             src="images/logo_wall_noir.png"
             alt="logo_wall"
