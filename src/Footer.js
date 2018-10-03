095659765b6414dee1d97b52b853d4caa7f1982e
Benoit Niveau <be.niveau@hotmail.fr>
2018-10-03 15:08:16 +0200

    logo and css footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,13 +2,22 @@ import React from "react";
 
 const Footer = () => (
   <footer>
-    <ul>
-      <li>
-        {" "}
-        <img src="images/logo_wall_noir.png" alt="logo_wall" />
-      </li>
-      <li>Made with Love by Quentin - Benoît - Corentin - Leuthsouline</li>
-    </ul>
+    <div class="container">
+      <ul class="row">
+        <li class="col-6">
+          {" "}
+          <img
+            src="images/logo_wall_noir.png"
+            alt="logo_wall"
+            className="logo"
+          />
+        </li>
+        <li class="col col-6 pt-3">
+          Made with <span class="heart">❤</span> by Quentin - Benoît - Corentin
+          - Leuthsouline
+        </li>
+      </ul>
+    </div>
   </footer>
 );
 
