bb305e2c98a9fc595d77ad4d7fe8857dd3695ce1
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-10-03 11:10:54 +0200

    erreur branche

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import ModalHelp from './modalhelp'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ModalHelp />
+        <a>-</a>
       </div>
     );
   }
