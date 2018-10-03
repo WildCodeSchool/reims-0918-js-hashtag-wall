5c0e098aa61f5dcd36c22796773f1689dbe917b5
Benoit Niveau <be.niveau@hotmail.fr>
2018-10-03 11:24:22 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Footer from "./Footer";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <a>-</a>
+        <Footer />
       </div>
     );
   }
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <ul>
+      <li>
+        {" "}
+        <img src="images/logo_wall_noir.png" alt="icone_wall" />
+        Wall
+      </li>
+      <li>Made with Love by Quentin - Benoît - Corentin - Leuthsouline</li>
+    </ul>
+  </footer>
+);
+
+export default Footer;
