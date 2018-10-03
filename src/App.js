21d984f335e8c4b7bbd57f9e1e7f44e42ea3fbe8
Benoit Niveau <be.niveau@hotmail.fr>
2018-10-03 17:34:05 +0200

    modif Header import

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import ModalHelp from "./ModalHelp";
 import HashtagInput from "./HashtagInput";
 import Footer from "./Footer";
+import Header from "./Header";
 
 class App extends Component {
   render() {
