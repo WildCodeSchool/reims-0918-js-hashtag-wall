43f658c5a0cdb1aa5f466cb154693ab98d989113
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-10-16 16:04:27 +0200

    startbuttonapi

--- src/App.js
+++ src/App.js
@@ -33,23 +33,18 @@ class App extends Component {
       isTweetPageDisplayed: false
     };
     this.handleClickNewButton = this.handleClickNewButton.bind(this);
-    this.handleClickStartButton = this.handleClickStartButton.bind(this);
   }
 
   getTweet = hashtag => {
     fetch(`https://safe-savannah-17783.herokuapp.com/?tag=${hashtag}`)
       .then(results => results.json()) // conversion du résultat en JSON
       .then(data => {
-        this.setState({ posts: tweetToPost(data) });
+        this.setState({ posts: tweetToPost(data), isTweetPageDisplayed: true });
       });
   };
 
   handleClickNewButton() {
     this.setState({ isTweetPageDisplayed: false });
-  }
-
-  handleClickStartButton() {
-    this.setState({ isTweetPageDisplayed: true });
   }
 
   handleInputContent = event => {
@@ -76,7 +71,6 @@ class App extends Component {
                   title={this.state.title}
                   onInputContent={this.handleInputContent}
                   getTweet={this.getTweet}
-                  tweetPageOnClick={this.handleClickStartButton}
                 />
               </Col>
             </Row>
@@ -90,21 +84,18 @@ class App extends Component {
           </Container>
         ) : (
           <Container fluid className="tweet" style={{ height: "100vh" }}>
-            <Row>
-              <Col xs="8" style={{ color: "white" }}>
-                <h1 className="mt-3" id="titleHashtag">
-                  #{this.state.title}
-                </h1>
-              </Col>
-              <Col xs="4" className="w-15 pb-3 text-right">
-                <Button
-                  className="mt-3"
-                  onClick={this.handleClickNewButton}
-                  color="primary"
-                >
-                  <p className="textButton ">#New</p>
-                </Button>
-              </Col>
+            <Row id="wallHeader" style={{ color: "white" }}>
+              <Button color="primary">
+                <p className="textButton ">#Top tweets</p>
+              </Button>
+
+              <h1 id="titleHashtag" className="mt-2">
+                #{this.state.title}
+              </h1>
+
+              <Button onClick={this.handleClickNewButton} color="primary">
+                <p className="textButton ">#New</p>
+              </Button>
             </Row>
             <CardColumns>
               {this.state.posts.map(post => (
