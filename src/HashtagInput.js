d25848ad68bc72aed875b56329a6d2cdcbe13121
Quentin Da Silva <quentin_51@live.fr>
2018-10-18 15:55:44 +0200

    del form

--- src/HashtagInput.js
+++ src/HashtagInput.js
@@ -6,39 +6,37 @@ import Loading from "./Loading";
 const HashtagInput = props => {
   return (
     <div>
-      <form>
-        <InputGroup size="lg">
-          <InputGroupAddon addonType="prepend">#</InputGroupAddon>
-          <Input
-            className="sizeInput"
-            value={props.title}
-            placeholder="Your Tag !"
-            onChange={props.onInputContent}
-            type="text"
-          />
-          <InputGroupAddon addonType="append">
-            <Button color="primary" onClick={props.onXClick}>
-              X
-            </Button>
-          </InputGroupAddon>
-        </InputGroup>
-        {!props.startLoad ? (
-          <div className="buttonPosition">
-            <Button
-              href="#titleHashtag"
-              className="w-25 mt-5"
-              color="primary"
-              onClick={() => props.getTweet(props.title)}
-            >
-              <p className="buttonText">#Start</p>
-            </Button>
-          </div>
-        ) : (
-          <div className="d-flex justify-content-center mt-5">
-            <Loading />
-          </div>
-        )}
-      </form>
+      <InputGroup size="lg">
+        <InputGroupAddon addonType="prepend">#</InputGroupAddon>
+        <Input
+          className="sizeInput"
+          value={props.title}
+          placeholder="Your Tag !"
+          onChange={props.onInputContent}
+          type="text"
+        />
+        <InputGroupAddon addonType="append">
+          <Button color="primary" onClick={props.onXClick}>
+            X
+          </Button>
+        </InputGroupAddon>
+      </InputGroup>
+      {!props.startLoad ? (
+        <div className="buttonPosition">
+          <Button
+            href="#titleHashtag"
+            className="w-25 mt-5"
+            color="primary"
+            onClick={() => props.getTweet(props.title)}
+          >
+            <p className="buttonText">#Start</p>
+          </Button>
+        </div>
+      ) : (
+        <div className="d-flex justify-content-center mt-5">
+          <Loading />
+        </div>
+      )}
     </div>
   );
 };
