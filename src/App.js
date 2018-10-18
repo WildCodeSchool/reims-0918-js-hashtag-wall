ba7ed8e735ac50c7520a1a22ecc6025ac71d6bc7
Benoit Niveau <be.niveau@hotmail.fr>
2018-10-18 09:45:25 +0200

    clear_input_onNew_click

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ class App extends Component {
   };
 
   handleClickNewButton() {
-    this.setState({ isTweetPageDisplayed: false });
+    this.setState({ isTweetPageDisplayed: false, title: "" });
   }
 
   handleXClick = event => {
