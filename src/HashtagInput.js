85844ab59f367ea65064292f0b9a161dea08c047
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-10-25 15:40:37 +0200

    filter tweet open when click on new

--- src/HashtagInput.js
+++ src/HashtagInput.js
@@ -40,8 +40,7 @@ const HashtagInput = props => {
               alt="logoTwitter"
             />
             <Button
-              id="startBtn"
-              href="#titleHashtag"
+              activeTab={props.activeTab}
               className="w-25 mt-5"
               color="primary"
               disabled={props.title.length === 0}
