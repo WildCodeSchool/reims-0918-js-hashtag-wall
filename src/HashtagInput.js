b617ae14f5b6465b132bc78b641b540616992d7f
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-10-25 15:42:31 +0200

    mistake deleted

--- src/HashtagInput.js
+++ src/HashtagInput.js
@@ -40,7 +40,6 @@ const HashtagInput = props => {
               alt="logoTwitter"
             />
             <Button
-              activeTab={props.activeTab}
               className="w-25 mt-5"
               color="primary"
               disabled={props.title.length === 0}
